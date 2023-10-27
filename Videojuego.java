fa7842be87a8d6fe6165513b4f204813d676e325
Ángel Gonzalez <angeladolfogonzalezarana@gmail.com>
2023-10-27 15:59:48 -0400

    comit

---
+++ Videojuego.java
@@ -0,0 +1,59 @@
+
+
+public class Videojuego {
+    //atributos
+
+    private int codigo;
+    private String titulo;
+    private String consola;
+    private int cantJugadores;
+    private String categoria;
+    
+    
+    //constructor vacio    
+    public Videojuego() {
+    }
+
+    //constructor
+    public Videojuego(int codigo, String titulo, String consola, int cantJugadores, String categoria) {
+        this.codigo = codigo;
+        this.titulo = titulo;
+        this.consola = consola;
+        this.cantJugadores = cantJugadores;
+        this.categoria = categoria;
+    }
+
+    //geters and seters
+    public int getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public String getConsola() {
+        return consola;
+    }
+    public void setConsola(String consola) {
+        this.consola = consola;
+    }
+    public int getCantJugadores() {
+        return cantJugadores;
+    }
+    public void setCantJugadores(int cantJugadores) {
+        this.cantJugadores = cantJugadores;
+    }
+    public String getCategoria() {
+        return categoria;
+    }
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    
+}
